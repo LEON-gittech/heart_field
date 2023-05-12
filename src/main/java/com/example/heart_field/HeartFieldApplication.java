7548fb62f71577570fbe43ac782a63cabbf0a70e
leonwang24 <2194369201@qq.com>
2023-05-12 10:19:49 +0800

    init

---
+++ src/main/java/com/example/heart_field/HeartFieldApplication.java
@@ -0,0 +1,13 @@
+package com.example.heart_field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartFieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartFieldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/heart_field/HeartFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heart_field;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartFieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
