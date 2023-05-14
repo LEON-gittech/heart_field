d4cb92a9e98b85887ae7a4926ecf71c57854dbda
Maydayahah <87710541+Maydayahah@users.noreply.github.com>
2023-05-15 00:36:17 +0800

    搭建MVC

---
+++ src/main/java/com/example/heart_field/service/impl/StaffStatService.java
@@ -0,0 +1,10 @@
+package com.example.heart_field.service.impl;
+
+import com.example.heart_field.entity.StaffStat;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StaffStatService extends IService<StaffStat> {
+
+}
