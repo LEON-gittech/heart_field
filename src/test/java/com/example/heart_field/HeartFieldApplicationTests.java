815fe6a6f5f4fc3e2a4ac4687c47eeccd86ac7b7
Alba Chen <863488096@qq.com>
2023-05-16 11:55:30 +0800

    加入redis相关配置

--- src/test/java/com/example/heart_field/HeartFieldApplicationTests.java
+++ src/test/java/com/example/heart_field/HeartFieldApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.heart_field;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -25,5 +29,29 @@ class HeartFieldApplicationTests {
         System.out.println(time.getDayOfYear());
         System.out.println(time.getDayOfWeek());
     }
+
+
+    //test for redis
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 向redis数据库中存储哈希类型的数据
+     */
+    @Test
+    void testRedisSet() throws Exception {
+        HashOperations ops=stringRedisTemplate.opsForHash();
+        ops.put("info","a","aa");
+    }
+
+    @Test
+    void testRedisGet() throws Exception {
+        HashOperations ops=stringRedisTemplate.opsForHash();
+        System.out.println(ops.get("info","a"));
+    }
+
+
+
 
 }
