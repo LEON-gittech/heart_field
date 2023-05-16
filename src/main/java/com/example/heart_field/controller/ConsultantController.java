1b78d54bb307d8687e9207a05cda00e1d213e6ad
leonwang24 <2194369201@qq.com>
2023-05-16 11:28:25 +0800

    feat(token): 实现token注解
    共有两个注解，一个是PassToken,一个是UserLoginToken
    注意：token是在header中的，我在apifox的测试目录下写了两个接口，对应apiController中的两个函数
    我提交的这几个文件你们都要同步更新，否则可能会出错
    新加了一个User类，不过这个类只对后端可见，为了解决不同表id重复的问题，在User中id为type+id，type为Integer，具体含义见注解

--- src/main/java/com/example/heart_field/controller/ConsultantController.java
+++ src/main/java/com/example/heart_field/controller/ConsultantController.java
@@ -1,14 +1,22 @@
 package com.example.heart_field.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.heart_field.entity.Consultant;
 import com.example.heart_field.service.ConsultantService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.heart_field.common.R;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
 @Slf4j
-@RestController("/consultants")
+@RestController
+@RequestMapping("/consultant")
 public class ConsultantController {
     @Autowired
     private ConsultantService consultantService;
@@ -18,10 +26,12 @@ public class ConsultantController {
      * @param page
      * @param pageSize
      * @param searchValue 搜索条件
+     * @param sortType 排序方式 0按照用户满意度 1按照帮助用户数 2表示是否空闲，默认为0
+     * @param sort 升序还是降序，0表示降序，1表示升序，默认为0
      * @return
      */
     @GetMapping
-    public R<Page>page(int page, int pageSize, String searchValue){
+    public R<Page>page(int page, int pageSize, String searchValue, int sortType, int sort){
         log.info("分类信息查询，page={},pageSize={},searchValue={}",page,pageSize,searchValue);
         //构造分页构造器
         Page<Consultant> pageinfo = new Page<>(page,pageSize);
@@ -38,6 +48,39 @@ public class ConsultantController {
                                 "concat('%',?,'%'))",searchValue))
         //JSON_CONTAINS函数用于判断json数组中是否包含某个元素
         ;
+
+        //按照sortType进行排序，并根据sort确认是升序还是降序
+        if(sort == 0){
+            switch (sortType){
+                case 0:
+                    queryWrapper.orderByDesc(Consultant::getRating);
+                    break;
+                case 1:
+                    queryWrapper.orderByDesc(Consultant::getHelpNum);
+                    break;
+                case 2:
+                    //这里因为空闲是0
+                    queryWrapper.orderByAsc(Consultant::getCurStatus);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else{
+            switch (sortType){
+                case 0:
+                    queryWrapper.orderByAsc(Consultant::getRating);
+                    break;
+                case 1:
+                    queryWrapper.orderByAsc(Consultant::getHelpNum);
+                    break;
+                case 2:
+                    queryWrapper.orderByDesc(Consultant::getCurStatus);
+                    break;
+                default:
+                    break;
+            }
+        }
         //执行查询
         consultantService.page(pageinfo,queryWrapper);
 
@@ -57,5 +100,9 @@ public class ConsultantController {
      *
      */
     @GetMapping("/preview")
-    public
+    public R<Consultant> preview(Long id){
+        log.info("咨询师信息查询，id={}",id);
+        Consultant consultant = consultantService.getById(id);
+        return R.success(consultant);
+    }
 }
